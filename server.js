bdd422c72bc373323e89b9b0d81c29d5f51b8ed4
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-10-25 20:18:48 -0500

    commit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
  
-app.get('/', (req, res) => {
+app.get('/MyWife', (req, res) => {
   res.send("Meissa Pyle");
 });
-
+app.get('/Myself', (req, res) => {
+  res.send("JT Pyle");
+});
 const port = 3000;
 app.listen(process.env.PORT || port);
 console.log('Web Server is listening at port ' + (process.env.port || port))
